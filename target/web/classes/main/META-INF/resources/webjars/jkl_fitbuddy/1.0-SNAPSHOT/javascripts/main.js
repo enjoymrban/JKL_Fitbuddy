8104c4e920ddf11a0c851e6e946dfc9d3dd362e3
enjoymrban <knecht.silvan@gmail.com>
2018-10-15 22:56:04 +0200

    Removing all files in .gitignore

--- target/web/classes/main/META-INF/resources/webjars/jkl_fitbuddy/1.0-SNAPSHOT/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZW5qb3ltcmJhbiIsImEiOiJjam5hY3EwcDQwZ2hiM3BwYWQ2dWt4a2x1In0.nlX1GeaPE2DQn3aZH0IJaA', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-}).addTo(mymap);
