847d756dfe343d6733f713841f08b15b9b9db6b7
Silvan Knecht <knecht.silvan@gmail.com>
2018-10-15 09:14:38 +0200

    added basic dependencies

--- build.sbt
+++ build.sbt
@@ -8,3 +8,12 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.12.6"
 
 libraryDependencies += guice
+
+
+libraryDependencies ++= Seq(
+  "org.webjars" % "bootstrap" % "4.0.0",
+  "org.webjars" % "jquery" % "3.2.1",
+  "org.webjars" % "popper.js" % "1.12.9",
+  "org.webjars" % "sammy" % "0.7.4",
+  "org.webjars" % "material-design-icons" % "3.0.1"
+)
---
+++ target/scala-2.12/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,15 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:C:/Users/Silvan Knecht/Documents/GitHub/JKL_Fitbuddy/conf/routes
+// @DATE:Mon Oct 15 09:05:42 CEST 2018
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
